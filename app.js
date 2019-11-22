51b2532675e7f8593d8f74620815d07aa981c4c7
canikwe <c.anikwe@gmail.com>
2019-11-22 10:43:54 -0500

    add and implement fetch and promise packages

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const Bluebird = require('bluebird')
+const fetch = require('node-fetch')
+fetch.promise = Bluebird
+
 app.get('/', (req, res) => {
   console.log(req.params)
   res.send([
@@ -9,6 +13,18 @@ app.get('/', (req, res) => {
     { id: 2, message: 'Chine says hello!' },
     { id: 3, message: 'So does Shannon 😀' }
   ])
+})
+
+app.get('/weather', (req, res) => {
+  console.log('Hit my weather route!')
+  fetch('https://www.metaweather.com/api/location/44418')
+  .then(resp => resp.json())
+  .then(resp => {
+    console.log('No errors here')
+    res.send(resp)
+  })
+  .catch(console.log)
+  // res.send({message: 'Hit my weather route'})
 })
 
 app.listen(port, () => {
