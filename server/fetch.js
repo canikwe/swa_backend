5d742a449991e60b6d4e83093a836d456303d23f
Chine Anikwe <c.anikwe@gmail.com>
2022-02-14 03:43:08 -0500

    run prettier and remove some console.logs

--- server/fetch.js
+++ server/fetch.js
@@ -1,37 +1,40 @@
-import Bluebird from 'bluebird'
-import fetch from 'node-fetch'
-import dotenv from 'dotenv'
+import Bluebird from "bluebird";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
 
 // Set up fetch and promise handling for async API calls
-fetch.promise = Bluebird
+fetch.promise = Bluebird;
 
 // Configure dotenv for environment variables
-dotenv.config()
+dotenv.config();
 
 // Base URL for weather API fetch calls
-const weatherURL = () => 'http://api.openweathermap.org/data/2.5/weather'
-const cityURL = () => 'https://api.opencagedata.com/geocode/v1/json?q='
+const weatherURL = () => "http://api.openweathermap.org/data/2.5/weather";
+const cityURL = () => "https://api.opencagedata.com/geocode/v1/json?q=";
 
-export const getByCity = location => {
-  return fetch(weatherURL() + `?q=${location}&` + 'APPID=' + process.env.WEATHER_API_KEY)
-  .then(resp => resp.json())
-}
+export const getByCity = (location) => {
+  return fetch(
+    weatherURL() + `?q=${location}&` + "APPID=" + process.env.WEATHER_API_KEY
+  ).then((resp) => resp.json());
+};
 
 export const getByLatLon = (lat, lon) => {
-  return fetch(weatherURL() + `?lat=${lat}&lon=${lon}&` + 'APPID=' + process.env.WEATHER_API_KEY)
-  .then(resp => resp.json())
-}
+  return fetch(
+    weatherURL() +
+      `?lat=${lat}&lon=${lon}&` +
+      "APPID=" +
+      process.env.WEATHER_API_KEY
+  ).then((resp) => resp.json());
+};
 
 export const getByCityId = (id) => {
-  return fetch(weatherURL() + `?id=${id}&` + 'APPID=' + process.env.WEATHER_API_KEY)
-  .then(resp => resp.json())
-}
+  return fetch(
+    weatherURL() + `?id=${id}&` + "APPID=" + process.env.WEATHER_API_KEY
+  ).then((resp) => resp.json());
+};
 
-export const searchCities = city => {
-  // return cityList.filter(cityObj => cityObj.name.toLowerCase() === 'springfield')
-  console.log(cityURL(), process.env.CITY_API_KEY)
-
-  return fetch(cityURL() + city + '&key=' + process.env.CITY_API_KEY)
-  .then(resp => resp.json())
-}
-
+export const searchCities = (city) => {
+  return fetch(cityURL() + city + "&key=" + process.env.CITY_API_KEY).then(
+    (resp) => resp.json()
+  );
+};
--- server/server.js
+++ server/server.js
@@ -1,126 +1,133 @@
-import express from 'express'
-import cors from 'cors'
-import { getByCity, getByLatLon, getByCityId, getCityInfo, searchCities } from './fetch' // Import fetch functions
+import express from "express";
+import cors from "cors";
+import {
+  getByCity,
+  getByLatLon,
+  getByCityId,
+  getCityInfo,
+  searchCities,
+} from "./fetch"; // Import fetch functions
 
 // Set up express to run on port 3000
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Apply express json parser to parse body from POST requests
-app.use(express.json())
+app.use(express.json());
 
 // Apply CORS exceptions for frontend API requests
-app.use(cors())
+app.use(cors());
 
 // Apply middleware to log method, path, and timestamp to the console
 app.use((req, res, next) => {
-  console.log("\x1b[36m", `${req.method} ${req.path} started at ${new Date()}`)
-  next()
-})
+  console.log("\x1b[36m", `${req.method} ${req.path} started at ${new Date()}`);
+  next();
+});
 
 // Root route to test connection
-app.get('/', (req, res) => {
-
+app.get("/", (req, res) => {
   res.status(200).json([
-    { id: 1, message: 'Hello World!' },
-    { id: 2, message: 'Chine says hello!' },
-    { id: 3, message: 'So does Shannon 😀' }
-  ])
-})
+    { id: 1, message: "Hello World!" },
+    { id: 2, message: "Chine says hello!" },
+    { id: 3, message: "So does Shannon 😀" },
+  ]);
+});
 
-app.get('/search/:location', (req, res) => {
-  searchCities(req.params.location)
-  .then(data => {
-    const cities = data.results.filter(c => {
-      console.log(c.components)
+app.get("/search/:location", (req, res) => {
+  searchCities(req.params.location).then((data) => {
+    const cities = data.results.filter((c) => {
+      console.log(c.components);
       switch (c.components._type) {
-        case 'city':
-          return true
-        case 'state':
-          return true
-        case 'village':
-          return true
-        case 'state_district':
-          return true
-        case 'village':
-          return true
-        case 'neighbourhood':
-          return true
-        case 'county':
-          return true
+        case "city":
+          return true;
+        case "state":
+          return true;
+        case "village":
+          return true;
+        case "state_district":
+          return true;
+        case "village":
+          return true;
+        case "neighbourhood":
+          return true;
+        case "county":
+          return true;
         default:
-          return false
+          return false;
       }
-    })
-    console.log(`${cities.length} out of ${data.results.length} cities returned`)
+    });
+    console.log(
+      `${cities.length} out of ${data.results.length} cities returned`
+    );
 
-    res.status(200).json(cities)
-  })
-})
+    res.status(200).json(cities);
+  });
+});
 
 // Single route to handle POST requests from frontend
-app.post('/weather', (req, res) => {
-  console.log(req.body.type)
-  switch(req.body.type) {
-    case 'location':
+app.post("/weather", (req, res) => {
+  console.log(req.body.type);
+  switch (req.body.type) {
+    case "location":
       getByCity(req.body.query)
-      .then(resp => {
-        console.log('Location case hit. No errors here')
-        res.status(200).json(resp)
-      })
-      .catch(console.log)
-      break
-    case 'coord':
+        .then((resp) => {
+          console.log("Location case hit. No errors here");
+          res.status(200).json(resp);
+        })
+        .catch(console.log);
+      break;
+    case "coord":
       getByLatLon(req.body.query.lat, req.body.query.lng)
-      .then(resp => {
-        console.log('Coordinates case hit. No errors here')
-        res.status(200).json(resp)
-      })
-      .catch(console.log)
-      break
+        .then((resp) => {
+          console.log("Coordinates case hit. No errors here");
+          res.status(200).json(resp);
+        })
+        .catch(console.log);
+      break;
     default:
-      console.log(`Response body ${res.body}`)
-      res.status(400).json({msg: 'Case statements not hit. Nothing to see here'})
+      console.log(`Response body ${res.body}`);
+      res
+        .status(400)
+        .json({ msg: "Case statements not hit. Nothing to see here" });
   }
-})
+});
 
 // Specific city is included in the URI path. API is queried for the *approximate* city
-app.get('/weather/:location', (req, res) => {
-  console.log('Hit my query route!')
+app.get("/weather/:location", (req, res) => {
+  console.log("Hit my query route!");
   getByCity(req.params.location)
-  .then(resp => {
-    console.log('/weather/:location route hit. No errors here')
-    res.status(200).json(resp)
-  })
-  .catch(console.log)
-})
+    .then((resp) => {
+      console.log("/weather/:location route hit. No errors here");
+      res.status(200).json(resp);
+    })
+    .catch(console.log);
+});
 
 // Latitude and longitude is included in the URI.
-app.get('/weather/:lat/:lon', (req, res) => {
-  console.log('lat/lon route')
+app.get("/weather/:lat/:lon", (req, res) => {
+  console.log("lat/lon route");
   getByLatLon(req.params.lat, req.params.lon)
-  .then(resp => {
-    console.log('/weather/:lat/:lon route hit. No errors here')
-    res.status(200).json(resp)
-  })
-  .catch(console.log)
-})
+    .then((resp) => {
+      console.log("/weather/:lat/:lon route hit. No errors here");
+      res.status(200).json(resp);
+    })
+    .catch(console.log);
+});
 
 // Custom error handling middleware
 app.use((err, req, res) => {
-  console.error(req.body)
-  console.log(res)
-  // res.status(500).json(err.stack)
-})
+  console.error(req.body);
+  console.log(res);
+});
 
 // Opens the server on the specified port
 const server = app.listen(PORT, () => {
-  console.log(`Simple Weather App currently listening on port: ${PORT}`)
-})
+  console.log(`Simple Weather App currently listening on port: ${PORT}`);
+});
 
 // Closes the server
-process.on('SIGINT', () => {
-  console.log('\n Gracefully stopping...')
-  server.close()
-  console.log('\n Have a nice day!')
-})
+process.on("SIGINT", () => {
+  console.log("\n Gracefully stopping...");
+  server.close();
+  console.log("\n Have a nice day!");
+});
